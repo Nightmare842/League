85a0912d800d08f088ed647f497ca351f046a550
mirin <mirin@MICHA-PC>
2023-01-18 23:09:02 +0100

    Projektdateien hinzufügen.

---
+++ League/Models/Spieler.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace League.Models
+{
+    class Spieler
+    {
+        private int id;
+        private string name;
+        private int age;
+        private string position;
+        private string team;
+        private string connectionString;
+
+        public Spieler(int id, string name, int age, string position, string team, string connectionString)
+        {
+            this.id = id;
+            this.name = name;
+            this.age = age;
+            this.position = position;
+            this.team = team;
+            this.connectionString = connectionString;
+            CreateDbIfNotExists();
+        }
+
+        public int Id
+        {
+            get { return id; }
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+
+        public string Position
+        {
+            get { return position; }
+            set { position = value; }
+        }
+
+        public string Team
+        {
+            get { return team; }
+            set { team = value; }
+        }
+
+        public void SaveToDb()
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                using (SqlCommand command = new SqlCommand())
+                {
+                    command.Connection = connection;
+                    command.CommandText = "INSERT INTO Players (Id, Name, Age, Position, Team) VALUES (@id, @name, @age, @position, @team)";
+                    command.Parameters.AddWithValue("@id", id);
+                    command.Parameters.AddWithValue("@name", name);
+                    command.Parameters.AddWithValue("@age", age);
+                    command.Parameters.AddWithValue("@position", position);
+                    command.Parameters.AddWithValue("@team", team);
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        private void CreateDbIfNotExists()
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                using (SqlCommand command = new SqlCommand())
+                {
+                    command.Connection = connection;
+                    command.CommandText = "IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'Players') CREATE TABLE Players (Id int, Name nvarchar(50), Age int, Position nvarchar(50), Team nvarchar(50))";
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
---
+++ League/Models/Team.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+class Team
+{
+    private int id;
+    private byte[] logo;
+    private string name;
+    private string league;
+    private string description;
+    private List<string> members;
+    private string connectionString;
+
+    public Team(int id, byte[] logo, string name, string league, string description, string connectionString)
+    {
+        this.id = id;
+        this.logo = logo;
+        this.name = name;
+        this.league = league;
+        this.description = description;
+        this.connectionString = connectionString;
+        this.members = GetMembersFromDb();
+    }
+
+    public void AddMember(string member)
+    {
+        members.Add(member);
+        AddMemberToDb(member);
+    }
+
+    public void RemoveMember(string member)
+    {
+        members.Remove(member);
+        RemoveMemberFromDb(member);
+    }
+
+    public int Id
+    {
+        get { return id; }
+    }
+
+    public byte[] Logo
+    {
+        get { return logo; }
+        set { logo = value; }
+    }
+
+    public string Name
+    {
+        get { return name; }
+        set { name = value; }
+    }
+
+    public string League
+    {
+        get { return league; }
+        set { league = value; }
+    }
+
+    public string Description
+    {
+        get { return description; }
+        set { description = value; }
+    }
+
+    public List<string> Members
+    {
+        get { return members; }
+    }
+
+    private void AddMemberToDb(string member)
+    {
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            connection.Open();
+            using (SqlCommand command = new SqlCommand())
+            {
+                command.Connection = connection;
+                command.CommandText = "INSERT INTO TeamMembers (TeamId, Member) VALUES (@teamId, @member)";
+                command.Parameters.AddWithValue("@teamId", id);
+                command.Parameters.AddWithValue("@member", member);
+                command.ExecuteNonQuery();
+            }
+        }
+    }
+
+    private void RemoveMemberFromDb(string member)
+    {
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            connection.Open();
+            using (SqlCommand command = new SqlCommand())
+            {
+                command.Connection = connection;
+                command.CommandText = "DELETE FROM TeamMembers WHERE TeamId = @teamId AND Member = @member";
+                command.Parameters.AddWithValue("@teamId", id);
+                command.Parameters.AddWithValue("@member", member);
+                command.ExecuteNonQuery();
+            }
+        }
+    }
+}
